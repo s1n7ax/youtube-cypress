e06d884d2c4bff5b7056e88efc1fd19a2b2447e0
s1n7ax <srineshnisala@gmail.com>
2020-05-27 04:34:35 +0530

    [ADD] external libraries example

---
+++ cypress/integration/external-libraries.test.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+// import path from 'path';
+// import Router from 'simple-message-router';
+
+// const express = require('express');
+// const app = express();
+
+describe('external libraries', () => {
+	it('should be able to use external libraries', () => {
+		// const p = path.join('/', 'home', 's1n7ax');
+		// console.log(p);
+		//
+		// var root = new Router();
+		//
+		// root.registerMiddleware(function() {
+		//     console.log('middleware');
+		// });
+		//
+		// root.dispatchRequest('/');
+		//
+		// app.get('/', function(req, res) {
+		//     res.send('Hello World');
+		// });
+		//
+		// app.listen(3000);
+	});
+});
