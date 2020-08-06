97061563318f60abef06c19fcc281fa701ae2eae
s1n7ax <srineshnisala@gmail.com>
2020-08-07 04:46:23 +0530

    [ADD] add loop part ii examples

--- cypress/integration/loop.test.js
+++ cypress/integration/loop.test.js
@@ -1,48 +1,66 @@
 /// <reference types="cypress" />
 
-let log = console.log;
-
 describe('loops', () => {
 	it('should loop', () => {
-		log('1');
-		cy.visit('https://www.google.com');
-		log('2');
-		cy.get('[name=q]').type('hello world');
-		log('3');
-
-		/* for loop */
-		// for (let i = 0; i < 3; i++) {
-		//     log('- ', i);
-		//     cy.then(() => log('* ', i));
-		// }
-
-		/* for each loop */
-		// cy.wrap(['a', 'b', 'c']).each((ele) => log(ele));
-
-		/* for each loop */
-		// cy.get('a').each((ele) => log(ele.text()));
-
-		/* community question 1 */
-		// let list = [];
+		// [...Array(20).keys()].every((index) => {
+		//     if (index >= 10) return false;
 		//
-		// for (let i = 0; i < 10; i++) {
-		//     cy.then(() => {
-		//         list.push(i);
-		//     });
-		// }
-		//
-		// log(list.length);
-		// cy.then(() => {
-		//     log(list.length);
+		//     console.log(index);
+		//     return true;
 		// });
 
-		/* community question 2 */
-		// let loop = true;
+		// cy.wrap([...Array(20).keys()]).each((index) => {
+		//     if (index >= 10) return false;
+		//     console.log(index);
+		// });
+
+		// const count = (index) => {
+		//     console.log(index++);
 		//
-		// while (loop) {
-		//     cy.get('[name=q]').then((ele) => {
-		//         if (ele.text() === 'hello world') loop = false;
-		//     });
-		// }
+		//     if (index < 10) count(index);
+		// };
+		// count(0);
+
+		cy.visit('https://mdbootstrap.com/docs/jquery/tables/pagination/');
+		findPerson('Sakura Yamamoto');
 	});
 });
+
+const findPerson = (name) => {
+	const findInPage = (index) => {
+		let found = false;
+
+		cy.get('.paginate_button:not(.previous):not(.next)').as('pages');
+
+		cy.get('@pages')
+			.its('length')
+			.then((len) => {
+				if (index >= len) {
+					return;
+				}
+
+				cy.get('@pages')
+					.eq(index)
+					.click();
+
+				cy.get('#dtBasicExample > tbody > tr > td:nth-child(1)')
+					.each((nameEle) => {
+						const nm = nameEle.text();
+						console.log(nm);
+						if (name === nm) {
+							found = true;
+							return false;
+						}
+					})
+					.then(() => {
+						if (!found) findInPage(++index);
+					});
+			});
+	};
+
+	findInPage(0);
+};
+
+// Cypress.on('uncaught:exception', () => {
+//     return false;
+// });
